606427a99788e40bb8c7809b8bc225659f735a6d
Thiago Pizzutti <tpizzutti@gmail.com>
2020-08-24 23:25:24 -0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import "./App.css"
+
+class App extends Component {
+  
+  render() {
+  return(
+    <>
+      <Navbar />
+      <div className="container">
+      <Users />
+      </div>
+
+    </>
+   
   );
+}
+
+  
 }
 
 export default App;
