56c326a208db069cbdd5213514728f8082eaaaba
Thiago Pizzutti <tpizzutti@gmail.com>
2020-08-24 23:25:45 -0300

    feat: create navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const Navbar = ({icon, title}) => {
+    
+    return (
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={icon} /> {title}
+          
+        </h1>
+     </nav>
+    )
+  
+}
+
+Navbar.defaultProps = {
+    title: 'GitFinder',
+    icon: "fab fa-github"
+  }
+
+  Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  }
+
+export default Navbar
