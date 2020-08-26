6c7eb4d7deacc7e11d38692542ffd568787d3a7d
Thiago Pizzutti <tpizzutti@gmail.com>
2020-08-25 22:24:25 -0300

    number of repo items per page

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ const App = () => {
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
       ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+      
    
     setUsers( res.data.items )
     setLoading( false )
@@ -44,7 +45,7 @@ const App = () => {
   const getUserRepos = async (username) => {
      setLoading( true ) 
     
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=20&sort=created:asc&client_id=
       ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
    
